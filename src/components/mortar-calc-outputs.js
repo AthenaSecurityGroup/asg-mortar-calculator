49fa52c740dfb98480ff7bd7b9d1d1ebb117584d
Jonathan M. Lane <jonathan@thinkingbig.net>
2016-03-12 15:45:39 -0400

    Modularize MortarCalculator Inputs and Outputs components

---
+++ src/components/mortar-calc-outputs.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class MortarCalculatorOutputs extends React.Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
--- src/components/mortar-calc.js
+++ src/components/mortar-calc.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Inputs from './mortar-calc-inputs.js';
+import Outputs from './mortar-calc-outputs.js';
 
 export default class MortarCalculator extends React.Component {
   render() {
@@ -6,36 +8,11 @@ export default class MortarCalculator extends React.Component {
       <div id="mortar-calculator">
         <div className="row">
           <div className="col-md-6">
-            <MortarCalculatorInputs/>
+            <Inputs/>
           </div>
 
-          <div className="col-md-6"></div>
-        </div>
-      </div>
-    );
-  }
-}
-
-class MortarCalculatorInputs extends React.Component {
-  render() {
-    return (
-      <div id="mortar-inputs">
-        <h2>Fire Support</h2>
-        <div className="form-group form-group-lg">
-          <label htmlFor="mortar-input-eastings">Support coordinates: eastings</label>
-          <input id="mortar-input-eastings" type="number" inputMode="numeric" className="form-control" placeholder="0000"/>
-        </div>
-
-        <div className="form-group form-group-lg">
-          <label htmlFor="mortar-input-northings">Support coordinates: northings</label>
-          <input id="mortar-input-northings" type="number" inputMode="numeric" className="form-control" placeholder="0000"/>
-        </div>
-
-        <div className="form-group form-group-lg">
-          <label>Support elevation</label>
-          <div className="input-group">
-            <input id="mortar-input-northings" type="number" inputMode="numeric" className="form-control" placeholder="0"/>
-            <span className="input-group-addon">meters</span>
+          <div className="col-md-6">
+            <Outputs/>
           </div>
         </div>
       </div>
@@ -43,4 +20,3 @@ class MortarCalculatorInputs extends React.Component {
   }
 }
 
-class MortarCalculatorOutputs extends React.Component { }
