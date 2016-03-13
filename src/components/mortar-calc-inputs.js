1ab1a4858a696f625f218175c0e0dc0e1f70708f
Jonathan M. Lane <jonathan@thinkingbig.net>
2016-03-13 16:34:54 -0300

    Updated labels and IDs on inputs for mortar-calc-inputs.js

--- src/components/mortar-calc-inputs.js
+++ src/components/mortar-calc-inputs.js
@@ -6,19 +6,37 @@ export default class MortarCalculatorInputs extends React.Component {
       <div id="mortar-inputs">
         <h2>Fire Support</h2>
         <div className="form-group form-group-lg">
-          <label htmlFor="mortar-input-eastings">Support coordinates: eastings</label>
-          <input id="mortar-input-eastings" type="number" inputMode="numeric" className="form-control" placeholder="0000"/>
+          <label htmlFor="mortar-input-support-eastings">Support coordinates: eastings</label>
+          <input id="mortar-input-support-eastings" type="number" inputMode="numeric" className="form-control" placeholder="0000"/>
         </div>
 
         <div className="form-group form-group-lg">
-          <label htmlFor="mortar-input-northings">Support coordinates: northings</label>
-          <input id="mortar-input-northings" type="number" inputMode="numeric" className="form-control" placeholder="0000"/>
+          <label htmlFor="mortar-input-support-northings">Support coordinates: northings</label>
+          <input id="mortar-input-support-northings" type="number" inputMode="numeric" className="form-control" placeholder="0000"/>
         </div>
 
         <div className="form-group form-group-lg">
-          <label htmlFor="mortar-input-elevation">Support elevation</label>
+          <label htmlFor="mortar-input-support-elevation">Support elevation</label>
           <div className="input-group">
-            <input id="mortar-input-elevation" type="number" inputMode="numeric" className="form-control" placeholder="0"/>
+            <input id="mortar-input-support-elevation" type="number" inputMode="numeric" className="form-control" placeholder="0"/>
+            <span className="input-group-addon">meters</span>
+          </div>
+        </div>
+
+        <div className="form-group form-group-lg">
+          <label htmlFor="mortar-input-target-eastings">Target coordinates: eastings</label>
+          <input id="mortar-input-target-eastings" type="number" inputMode="numeric" className="form-control" placeholder="0000"/>
+        </div>
+
+        <div className="form-group form-group-lg">
+          <label htmlFor="mortar-input-target-northings">Target coordinates: northings</label>
+          <input id="mortar-input-target-northings" type="number" inputMode="numeric" className="form-control" placeholder="0000"/>
+        </div>
+
+        <div className="form-group form-group-lg">
+          <label htmlFor="mortar-input-target-elevation">Target elevation</label>
+          <div className="input-group">
+            <input id="mortar-input-target-elevation" type="number" inputMode="numeric" className="form-control" placeholder="0"/>
             <span className="input-group-addon">meters</span>
           </div>
         </div>
