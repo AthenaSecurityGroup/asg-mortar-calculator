d8195ce62d102d7032f8074bdffdd95759ce33b3
Jonathan M. Lane <jonathan@thinkingbig.net>
2016-03-13 16:35:41 -0300

    Mortar Outputs component initial mock up

--- src/components/mortar-calc-outputs.js
+++ src/components/mortar-calc-outputs.js
@@ -3,7 +3,29 @@ import React from 'react';
 export default class MortarCalculatorOutputs extends React.Component {
   render() {
     return (
-      <div></div>
+      <div id="mortar-outputs">
+        <h2>Output</h2>
+        <div className="form-group form-group-lg">
+          <label htmlFor="mortar-output-bearing">Bearing</label>
+          <div className="input-group">
+            <input id="mortar-output-bearing" type="number" className="form-control" placeholder="0" readOnly/>
+            <span className="input-group-addon"><abbr title="degrees">&deg;</abbr></span>
+          </div>
+        </div>
+
+        <div className="form-group form-group-lg">
+          <label htmlFor="mortar-output-ranging">Ranging</label>
+          <div className="input-group">
+            <input id="mortar-output-ranging" type="number" className="form-control" placeholder="0" readOnly/>
+            <span className="input-group-addon"><abbr title="degrees">&deg;</abbr></span>
+          </div>
+        </div>
+
+        <div className="form-group form-group-lg">
+          <label htmlFor="mortar-output-charge">Charge</label>
+          <input id="mortar-output-charge" type="text" className="form-control" placeholder="C1" readOnly/>
+        </div>
+      </div>
     );
   }
 }
